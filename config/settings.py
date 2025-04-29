6e72565f9d5bf527a485f232e47894c72638c9bd
ElizaSwanson <Liz99246@gmail.com>
2025-04-29 17:12:59 +0300

    35.2 updates111

--- config/settings.py
+++ config/settings.py
@@ -155,7 +155,7 @@ if CACHE_ENABLED:
     CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://localhost:6379/1',
+        'LOCATION': 'redis://redis:6379/1',
     }
 }
 
@@ -183,12 +183,14 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://localhost:8080',
+    'http://51.250.25.183/'
 ]
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://localhost:8080',
+    'http://51.250.25.183/'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
